1d10b2213c6e616ad6b25c5936248e60a3c6083b
VietTQ20 <viettq20@fpt.com.vn>
2021-02-22 17:38:22 +0700

    commit all

--- src/index.js
+++ src/index.js
@@ -17,12 +17,17 @@ const config = {
   }
 };
 
-new Phaser.Game(config);
-
 function preload () {
   this.load.image('sky', 'assets/sky.png');
+  this.load.image('bird', 'assets/bird.png');
 }
 
 function create () {
-  this.add.image(400, 300, 'sky');
+  // trục x, trục y, key value of image
+  this.add.image(0, 0, 'sky').setOrigin(0);
+  // middle of the height , 1/10 width
+  bird = this.add.sprite(config.width * 0.1, config.height / 2, 'bird').setOrigin(0)
+  debugger
 }
+
+new Phaser.Game(config);
