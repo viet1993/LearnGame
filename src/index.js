247e89d7fc1474e8548b877a24d1ac4abcaa9264
VietTQ20 <viettq20@fpt.com.vn>
2021-02-25 17:30:31 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -8,14 +8,22 @@ const config = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 200 }
+      gravity: { y: 400 },
+      debug: true
     }
   },
   scene: {
-    preload: preload,
-    create: create
+    preload,
+    create,
+    update
   }
 };
+
+const VELOCITY = 200;
+
+let bird = null;
+let flapVelocity = 250;
+let totalDelta = null;
 
 function preload () {
   this.load.image('sky', 'assets/sky.png');
@@ -26,8 +34,25 @@ function create () {
   // trục x, trục y, key value of image
   this.add.image(0, 0, 'sky').setOrigin(0);
   // middle of the height , 1/10 width
-  bird = this.add.sprite(config.width * 0.1, config.height / 2, 'bird').setOrigin(0)
-  debugger
+  bird = this.physics.add.sprite(config.width * 0.1, config.height / 2, 'bird').setOrigin(0);
+  
+  this.input.on('pointerdown',flap);
+  this.input.keyboard.on('keydown_SPACE', flap);
+}
+
+// if bird position x is same or larger than width of canvas go back the left
+// if bird position x is smaller or equal to 0 then move back to the right
+function update(time, delta) {
+  if(bird.x >= config.width - bird.width) {
+    bird.body.velocity.x = -VELOCITY;
+  }
+  else if (bird.x <= 0) {
+    bird.body.velocity.x = VELOCITY;
+  }
+}
+
+function flap () {
+  bird.body.velocity.y = -flapVelocity;
 }
 
 new Phaser.Game(config);
