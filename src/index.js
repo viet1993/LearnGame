1526c84a78d61202002679209d0920c9cb93e458
Aikawa1993 <viettran47@gmail.com>
2021-02-28 22:28:46 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const VELOCITY = 200;
 
 let bird = null;
 let flapVelocity = 250;
-let totalDelta = null;
+const initialBirdPosition = { x: config.width * 0.1, y: config.height / 2}
 
 function preload () {
   this.load.image('sky', 'assets/sky.png');
@@ -34,7 +34,7 @@ function create () {
   // trục x, trục y, key value of image
   this.add.image(0, 0, 'sky').setOrigin(0);
   // middle of the height , 1/10 width
-  bird = this.physics.add.sprite(config.width * 0.1, config.height / 2, 'bird').setOrigin(0);
+  bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird').setOrigin(0);
   
   this.input.on('pointerdown',flap);
   this.input.keyboard.on('keydown_SPACE', flap);
@@ -43,19 +43,15 @@ function create () {
 // if bird position x is same or larger than width of canvas go back the left
 // if bird position x is smaller or equal to 0 then move back to the right
 function update(time, delta) {
-  if(bird.x >= config.width - bird.width) {
-    bird.body.velocity.x = -VELOCITY;
-  }
-  else if (bird.x <= 0) {
-    bird.body.velocity.x = VELOCITY;
+  if (bird.y > config.height || bird.y < 0 ) {
+    bird.x = initialBirdPosition.x;
+    bird.y = initialBirdPosition.y;
   }
 }
 
 function flap () {
   bird.body.velocity.y = -flapVelocity;
-  bird = this.physics.add.sprite(config.width * 0.1, config.height / 2, 'bird').setOrigin(0)
-
-  bird.body.gravity.y = 200
+  console.log(bird.y)
   //debugger
 }
 
