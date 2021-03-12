5535048f2d629ff4003dfbcfd75614f8a799f281
VietTQ20 <viettq20@fpt.com.vn>
2021-03-12 17:19:45 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import Phaser from "phaser";
 
 const config = {
   type: Phaser.AUTO,
-  width: 2800,
+  width: 800,
   height: 600,
   physics: {
     default: 'arcade',
@@ -22,12 +22,10 @@ const VELOCITY = 200;
 const PIPES_TO_RENDER = 4;
 
 let bird = null;
-let upperPipe = null;
-let lowerPipe = null;
-
+let pipes = null;
+// khoảng cách giữa 2 ống nước trong khoảng 150 - 250
 const pipeVertaicalDistanceRange = [150, 250];
-let pipeHorizontalDistance = 250;
-let pipeVertaicalDistance = Phaser.Math.Between(pipeVertaicalDistanceRange[0], pipeVertaicalDistanceRange[1]);
+const pipeHorizonDistanceRange = [400, 450];
 
 let flapVelocity = 250;
 const initialBirdPosition = { x: config.width * 0.1, y: config.height / 2}
@@ -46,13 +44,17 @@ function create () {
   bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird').setOrigin(0);
   bird.body.gravity.y = 400;
 
+  pipes = this.physics.add.group();
+
   for (let i = 0; i < PIPES_TO_RENDER; i++) {
     // gắn vị trí ông nước ban đầu
-    upperPipe = this.physics.add.sprite(0, 0, 'pipe').setOrigin(0, 1);
-    lowerPipe = this.physics.add.sprite(0, 0, 'pipe').setOrigin(0, 0);
+    let upperPipe = pipes.create(0, 0, 'pipe').setOrigin(0, 1);
+    let lowerPipe = pipes.create(0, 0, 'pipe').setOrigin(0, 0);
 
     placePipe(upperPipe, lowerPipe);
   }
+
+  pipes.setVelocityX(-VELOCITY);
   
   this.input.on('pointerdown',flap);
   this.input.keyboard.on('keydown_SPACE', flap);
@@ -64,6 +66,8 @@ function update(time, delta) {
   if (bird.y > config.height || bird.y < - bird.height ) {
     restartBirdPosition();
   }
+  
+  recyclePipes();
 }
 
 function restartBirdPosition () {
@@ -72,26 +76,44 @@ function restartBirdPosition () {
 }
 
 function placePipe (uPipe, lPipe) {
-  pipeHorizontalDistance += 400;
-  let pipeVerticalDistance = Phaser.Math.Between(...pipeVertaicalDistanceRange);
-  let pipeVerticalPosition = Phaser.Math.Between(50, config.height - 50 - pipeVerticalDistance);
-  
-  // gắn vị trí ông nước trên phia đầu
-  uPipe.x = pipeHorizontalDistance;
-  uPipe.y = pipeVerticalDistance;
+  const rightMostX = getRightMostPipe();
+  const pipeVerticalDistance = Phaser.Math.Between(...pipeVertaicalDistanceRange);
+  // 50 là chiều cao tối thiểu từ frame game đến mép thành ống nước
+  const pipeVerticalPosition = Phaser.Math.Between(50, config.height - 50 - pipeVerticalDistance);
+  const pipeHorizontalDistance = Phaser.Math.Between(...pipeHorizonDistanceRange)
 
+  // gắn vị trí ông nước trên phia đầu
+  uPipe.x = rightMostX + pipeHorizontalDistance;
+  uPipe.y = pipeVerticalPosition;
+  // gắn vị trí ông nước trên phia dưới
   lPipe.x = uPipe.x;
-  lPipe.y =  uPipe.y + pipeVerticalDistance;
-  
-  // vận tốc ống nước
-  uPipe.body.velocity.x = -200;
-  lPipe.body.velocity.x = -200;
+  lPipe.y = uPipe.y + pipeVerticalDistance;
+}
+
+function recyclePipes() {
+  const tempPipes = [];
+  pipes.getChildren().forEach(pipe => {
+    if (pipe.getBounds().right <= 0) {
+      // recycle pipe
+      tempPipes.push(pipe);
+      if (tempPipes.length === 2) {
+        placePipe(...tempPipes);
+      }
+    }
+  })
+}
+
+// tính toán khoảng cách ngẫu nhiên của ống nước
+function getRightMostPipe() {
+  let rightMostX = 0;
+  pipes.getChildren().forEach(pipe => {
+    rightMostX = Math.max(pipe.x, rightMostX);
+  });
+  return rightMostX;
 }
 
 function flap () {
   bird.body.velocity.y = -flapVelocity;
-  console.log(bird.y)
-  //debugger
 }
 
 
